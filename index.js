8d8a36a639d9db71e7260e3e34ccb9346dad80e2
buzzdeptrai <42234448+buzzdeptrai@users.noreply.github.com>
2022-12-05 14:12:18 +0700

    update index

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    
-    console.log("get home")
-  res.send('Hello World!')
-    
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
