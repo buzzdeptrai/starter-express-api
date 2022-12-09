9219ed6cb485e5871c5728c1678b3df8224e27c7
buzzdeptrai <vuquocvietkg@gmail.com>
2022-12-09 11:32:26 +0700

    update new ejs

---
+++ src/configs/viewEngine.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+//config view engine for an express app
+let configViewEngine = (app) => {
+    app.use(express.static('./src/public'))
+    app.set("view engine", "ejs");
+    app.set("views","./src/views");
+};
+module.exports = configViewEngine;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+
+const viewEngine = require("./configs/viewEngine");
+
 const app = express();
 const port = 3000;
 
+viewEngine(app);
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/home", (req, res) => {
+  res.render("homepage.ejs");
 });
 
 app.listen(port, () => {
