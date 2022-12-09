26a275def51485fc28cfb2b4f7fdfa1e265356c3
buzzdeptrai <vuquocvietkg@gmail.com>
2022-12-09 13:54:18 +0700

    demo mess upate332312

--- src/index.js
+++ src/index.js
@@ -33,40 +33,108 @@ app.get("/webhook", function (req, res) {
 });
 
 app.post("/webhook", function (req, res) {
-  // Phần sử lý tin nhắn của người dùng gửi đến
-  var entries = req.body.entry;
-  for (var entry of entries) {
-    var messaging = entry.messaging;
-    for (var message of messaging) {
-      var senderId = message.sender.id;
-      if (message.message) {
-        if (message.message.text) {
-          var text = message.message.text;
-          sendMessage(senderId, "Hello!! I'm a bot. Your message: " + text);
-        }
-      }
-    }
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === "page") {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      // if (webhook_event.message) {
+      //   handleMessage(sender_psid, webhook_event.message);
+      // } else if (webhook_event.postback) {
+      //   handlePostback(sender_psid, webhook_event.postback);
+      // }
+
+      handleMessage(sender_psid, webhook_event.message);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
   }
-  res.status(200).send("OK");
 });
 
-// Đây là function dùng api của facebook để gửi tin nhắn
-function sendMessage(senderId, message) {
-  request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {
-      access_token: PAGE_ACCESS_TOKEN,
-    },
-    method: "POST",
-    json: {
-      recipient: {
-        id: senderId,
+async function handleMessage(sender_psid, received_message) {
+  let response;
+
+  /// Checks if the message contains text
+  response = await {
+    attachment: {
+      type: "template",
+      payload: {
+        template_type: "generic",
+        elements: [
+          {
+            title: "Welcome to Bot",
+            subtitle: "Tap a button to answer.",
+            buttons: [
+              {
+                type: "postback",
+                title: "Show Product",
+                payload: "SHOW_CART",
+              },
+              {
+                type: "postback",
+                title: "View Cart",
+                payload: "REVIEW_CART",
+              },
+
+              {
+                type: "web_url",
+                title: "Web Shop",
+                url: "https://facebook-bot-buz.herokuapp.com/facebook_cart",
+                webview_height_ratio: "full",
+                messenger_extensions: true,
+              },
+            ],
+          },
+        ],
       },
-      message: {
-        text: message,
-      },
     },
-  });
+  };
+
+  // Send the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
 }
 
 app.listen(port, () => {
